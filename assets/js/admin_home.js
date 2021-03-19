daade4e49683597a239a51cf2ec56128f55ea1f7
soumyadeepsp <soumyadeepsp@gmail.com>
2021-03-19 23:11:43 +0530

    updated

--- assets/js/admin_home.js
+++ assets/js/admin_home.js
@@ -52,24 +52,58 @@ function sendMessage(e) {
   message.innerHTML = dues;
 }
 
-studentList = [{
-  'name' : 'Soumyadeep Paul',
-  'email' : 'soumyadeep18104@iiitd.ac.in',
-  'roll' : '2018104'
-}, {
-  'name' : 'Yashdeep Prasad',
-  'email' : 'yashdeep18121@iiitd.ac.in',
-  'roll' : '2018121'
-}, {
-  'name' : 'Himanshu Aggarwal',
-  'email' : 'himanshu18146@iiitd.ac.in',
-  'roll' : '2018146'
-}, {
-  'name' : 'Vishwesh',
-  'email' : 'vishwesh18084@iiitd.ac.in',
-  'roll' : '2018084'
-}];
-
-for (var i=0; i<studentList.length; i++) {
-  
-}
+// var accordion = document.getElementsByClassName('accordion')[0];
+// console.log(studentList);
+// for (var i=0; i<studentList.length; i++) {
+//   console.log(studentList[i]);
+//   var accordion_item = document.createElement('div');
+//   accordion_item.classList.add('accordion-item');
+//   var button = document.createElement('button');
+//   button.setAttribute('id', 'accordion-button-1');
+//   button.setAttribute('aria-expanded', "false");
+//   accordion_item.appendChild(button);
+//   var span = document.createElement('span');
+//   span.classList.add('accordion-title');
+//   span.innerHTML = `${studentList[i].email} - ${studentList[i].roll}`;
+//   button.appendChild(span);
+//   var i = document.createElement('i');
+//   i.setAttribute('class', 'fas fa-check-circle send_request');
+//   // i.setAttribute('onclick', `${event.stopPropagation() ;approved(this)}`);
+//   // i.addEventListener('click', (e) => {
+//   //   e.stopPropagation();
+//   //   approved(this)
+//   // });
+//   button.appendChild(i);
+//   var span2 = document.createElement('span');
+//   span2.setAttribute('class', 'icon');
+//   span2.setAttribute('aria-hidden', 'true');
+//   button.appendChild(span2);
+//   var accordion_content = document.createElement('div');
+//   accordion_content.classList.add('accordion-content');
+//   accordion_item.appendChild(accordion_content);
+//   var div2 = document.createElement('div');
+//   div2.setAttribute('class', 'input-group mb-3');
+//   accordion_content.appendChild(div2);
+//   var message_input = document.createElement('input');
+//   message_input.setAttribute('type', 'text');
+//   message_input.setAttribute('class', 'form-control');
+//   message_input.setAttribute('placeholder', 'Send a message ...');
+//   message_input.setAttribute('aria-label', "Recipient's username");
+//   message_input.setAttribute('aria-describedby', 'basic-addon2');
+//   div2.appendChild(message_input);
+//   var div3 = document.createElement('div');
+//   div3.classList.add('input-group-append');
+//   div2.appendChild(div3);
+//   var send = document.createElement('i');
+//   send.setAttribute('class', 'fas fa-paper-plane send');
+//   // send.setAttribute('onclick', `${sendMessage(event)}`);
+//   // send.addEventListener('click', (e) => {
+//   //   sendMessage(e);
+//   // });
+//   div3.appendChild(send);
+//   var message_text = document.createElement('span');
+//   message_text.classList.add('message');
+//   message_text.innerHTML = 'You have not sent any message currently.';
+//   accordion_content.appendChild(message_text);
+//   accordion.appendChild(accordion_item);
+// }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,8 +5,26 @@ module.exports.home = (req, res) => {
 }
 
 module.exports.adminHome = (req, res) => {
+    var studentList = [{
+        'name' : 'Soumyadeep Paul',
+        'email' : 'soumyadeep18104@iiitd.ac.in',
+        'roll' : '2018104'
+      }, {
+        'name' : 'Yashdeep Prasad',
+        'email' : 'yashdeep18121@iiitd.ac.in',
+        'roll' : '2018121'
+      }, {
+        'name' : 'Himanshu Aggarwal',
+        'email' : 'himanshu18146@iiitd.ac.in',
+        'roll' : '2018146'
+      }, {
+        'name' : 'Vishwesh',
+        'email' : 'vishwesh18084@iiitd.ac.in',
+        'roll' : '2018084'
+      }];
     return res.render('admin_home', {
-        'title': 'Admin - Home'
+        'title': 'Admin - Home',
+        'studentList': studentList
     })
 }
 
