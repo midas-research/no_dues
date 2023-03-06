f4a1c4d473957e9686179b0dcce80eb2fbe448dd
bhavya20428 <bhavya20428@iiitd.ac.in>
2023-03-06 17:59:00 +0530

    Flickering Screen

--- assets/layout.js
+++ assets/layout.js
@@ -2,7 +2,7 @@ const navbar = document.getElementById("navbar");
 
 var topIdx = navbar.offsetTop;
 function stickynavbar() {
-  if (window.scrollY > topIdx+(window.innerHeight/10)) {
+  if (window.scrollY >= topIdx) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
